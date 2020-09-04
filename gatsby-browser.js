17efa53c0f779faa2fca7d3be4dc418f99383448
Nate Arnold <hello@natearnold.me>
2020-09-04 17:18:58 -0500

    Remove comments in gatsby-browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,7 +4,6 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-// You can delete this file if you're not using it
 export const onClientEntry = async () => {
   if (typeof IntersectionObserver === "undefined") {
     await import("intersection-observer")
